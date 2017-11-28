a05d323963fc6da2f16e1a6c0fe18d2850ec5a18
PJH <zzz6866@gmail.com>
2017-11-28 17:32:15 +0900

    테스트 추가

--- src/main/java/kr/co/sibal001/HelloController.java
+++ src/main/java/kr/co/sibal001/HelloController.java
@@ -1,21 +1,29 @@
 package kr.co.sibal001;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import static org.springframework.web.bind.annotation.RequestMethod.GET;
 
 /**
  * Created by pjh on 2017-11-28.
  */
-@RestController
+//@RestController
+@Controller
 public class HelloController {
     @RequestMapping("/")
     public String hello() {
         return "Hello, Spring Boot!";
     }
 
-    @RequestMapping("/test")
-    public String test() {
-        return "Hello, Spring Boot TEST!";
+    @RequestMapping("/greeting/{name}")
+    public String greeting(@PathVariable(value = "name", required = false) String name, Model model) {
+        model.addAttribute("name", name);
+        return "greeting";
     }
 }
 
