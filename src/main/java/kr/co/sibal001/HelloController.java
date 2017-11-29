708ed92c2bb2c7182cf627e98dd5b48cf0bf7307
Mr. park <zzz6866@gmail.com>
2017-11-29 09:01:25 +0900

    최신화 커밋

--- src/main/java/kr/co/sibal001/HelloController.java
+++ src/main/java/kr/co/sibal001/HelloController.java
@@ -1,13 +1,11 @@
 package kr.co.sibal001;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
-
-import static org.springframework.web.bind.annotation.RequestMethod.GET;
 
 /**
  * Created by pjh on 2017-11-28.
@@ -15,6 +13,8 @@ import static org.springframework.web.bind.annotation.RequestMethod.GET;
 //@RestController
 @Controller
 public class HelloController {
+    private static final Logger log = LoggerFactory.getLogger(HelloController.class);
+
     @RequestMapping("/")
     public String hello() {
         return "Hello, Spring Boot!";
@@ -23,6 +23,7 @@ public class HelloController {
     @RequestMapping("/greeting/{name}")
     public String greeting(@PathVariable(value = "name", required = false) String name, Model model) {
         model.addAttribute("name", name);
+        log.debug("name = " + name);
         return "greeting";
     }
 }
