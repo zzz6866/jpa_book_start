f162fda12ab41092fe781f338ffe0d30a52f4985
Mr. park <zzz6866@gmil.com>
2017-12-12 15:24:57 +0900

    중간 커밋

--- src/main/java/kr/co/sibal/Application.java
+++ src/main/java/kr/co/sibal/Application.java
@@ -1,9 +1,7 @@
-package kr.co.sibal.controller;
+package kr.co.sibal;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Configuration;
 
 @SpringBootApplication
 //@Configuration
